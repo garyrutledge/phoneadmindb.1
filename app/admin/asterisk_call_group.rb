9a4a80cc7e707fbe227b5b842b430e992a133323
Prelang Builder <builder@prelang.com>
2015-05-07 17:37:39 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'AsteriskCallGroup' to allow for create, update

--- app/admin/asterisk_call_group.rb
+++ app/admin/asterisk_call_group.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register AsteriskCallGroup do
+  permit_params :group, :members
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
