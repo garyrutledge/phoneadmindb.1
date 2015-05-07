58e2fe46276a7a2dd62d17cec3ba3f2f1c5e2b0d
Prelang Builder <builder@prelang.com>
2015-05-07 17:37:07 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'AsteriskVoicemailBox' to allow for create, update

--- app/admin/asterisk_voicemail_box.rb
+++ app/admin/asterisk_voicemail_box.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register AsteriskVoicemailBox do
+  permit_params :prefix, :extension, :did, :display_name, :password, :email
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
